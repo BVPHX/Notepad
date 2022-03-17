a8a3022e97ab32a956cd0e0a6348505b7290337a
ИСП-31 <-31@LAB24-07>
2022-03-17 09:54:30 +0300

    Добавьте файлы проекта.

---
+++ Notepad/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isSaved = false;
+        bool isChangesSaved = false;
+        public string FileName { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            textField.CanUndo = true;
+            FileName = "Безымянный";
+            Title = FileName + " " + "-" + " " + "Блокнот";
+
+        }
+
+
+
+        private void MenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void StatusBarOnOff(object sender, RoutedEventArgs e)
+        {
+            if (statBatItem.IsChecked == true) statusBar.Visibility = Visibility.Visible;
+            if (statBatItem.IsChecked == false) statusBar.Visibility = Visibility.Hidden;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (isChangesSaved == false)
+            {
+                var result = MessageBox.Show("Сохранить изменения?", "Изменения не сохранены", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes) SaveOpen.Save(textField.Document.ToString());
+            }
+            if (isSaved == false)
+            {
+                var result = MessageBox.Show("Документ не сохранён. Сохранить документ?", "Файл не сохранен", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes) SaveOpen.SaveAs(textField., out bool issaved, out string name);
+                textField.SaveFile();
+            }
+        
+
+        }
+
+        private void textField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            isChangesSaved = false;
+        }
+    }
+}
---
+++ Notepad/SaveOpen.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Notepad
+{
+    class SaveOpen
+    {
+        public static void SaveAs(string text, out bool issaved, out string name)
+        {
+            issaved = false;
+            name = string.Empty;
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Текстовые документы (*.rtf)|*.rtf|Все файлы (*.*)|*.rtf*";
+            dialog.FilterIndex = 2;
+            dialog.RestoreDirectory = true;
+            if (dialog.ShowDialog() == true)
+            {
+                StreamWriter _stream = new StreamWriter(dialog.FileName);
+                _stream.WriteLine(dialog.SafeFileName);
+                _stream.Write(text);
+                    // Code to write the stream goes here.
+                    _stream.Close();
+                issaved = true;
+                name = dialog.SafeFileName ;
+            }
+
+
+
+            else issaved = false;
+
+        }
+        public static void Save(string text)
+        {
+
+        }
+        public void Open(out string text, out string name)
+        {
+
+            text = string.Empty;
+            name = string.Empty;
+        }
+    }
+}
